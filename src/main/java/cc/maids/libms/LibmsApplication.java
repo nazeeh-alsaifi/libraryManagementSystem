f7a8163aad52a1fda547823770f7687366c61077
nazeeh-alsaifi <nazeeh.saifi@gmail.com>
2024-01-09 23:05:23 +0400

    init new spring boot project

---
+++ src/main/java/cc/maids/libms/LibmsApplication.java
@@ -0,0 +1,13 @@
+package cc.maids.libms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/maids/libms/LibmsApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.maids.libms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
