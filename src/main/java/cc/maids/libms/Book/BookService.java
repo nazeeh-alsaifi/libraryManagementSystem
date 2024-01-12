54b426e74dd58e3f47ad192a329e2f066a988684
nazeeh-alsaifi <nazeeh.saifi@gmail.com>
2024-01-13 00:48:51 +0400

    caching the findById for both Entities and evicting on update or delete

--- src/main/java/cc/maids/libms/Book/BookService.java
+++ src/main/java/cc/maids/libms/Book/BookService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,21 +16,23 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
-    @Cacheable("books")
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
 
     }
 
+    @Cacheable("books")
     public Optional<Book> getBookById(long id) {
         return bookRepository.findById(id);
     }
 
+    @CacheEvict(value = "books", key = "#book.id")
     @Transactional
     public Book saveBook(Book book) {
         return bookRepository.save(book);
     }
 
+    @CacheEvict(value = "books")
     @Transactional
     public void deleteBook(Long id) {
         bookRepository.deleteById(id);
--- src/main/java/cc/maids/libms/Patron/PatronService.java
+++ src/main/java/cc/maids/libms/Patron/PatronService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,21 +15,23 @@ public class PatronService {
     @Autowired
     private PatronRepository patronRepository;
 
-    @Cacheable("patrons")
     public List<Patron> getAllPatrons() {
         return patronRepository.findAll();
 
     }
 
+    @Cacheable("patrons")
     public Optional<Patron> getPatronById(long id) {
         return patronRepository.findById(id);
     }
 
+    @CacheEvict(value = "patrons", key = "#patron.id")
     @Transactional
     public Patron savePatron(Patron patron) {
         return patronRepository.save(patron);
     }
 
+    @CacheEvict("patrons")
     @Transactional
     public void deletePatron(Long id) {
         patronRepository.deleteById(id);
